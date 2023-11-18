438e407a6856864e949a081599222dc49f78f886
laleeroy <gabwielcastwo@gmail.com>
2023-11-18 23:23:33 +0530

    sm8250-common: Build vendor.display.config@1.9 getTransport: Cannot find entry vendor.display.config@1.9::IDisplayConfig/default in either framework or device VINTF manifest.
    [ERROR][FORMAT ] camxdisplayconfig.cpp:124 Initialize() IDisplayConfig service is NULL, version 1.9

--- kona.mk
+++ kona.mk
@@ -176,6 +176,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.memtrack-service
 
+PRODUCT_PACKAGES += \
+    vendor.display.config@1.9 \
+    vendor.display.config@1.9.vendor \
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm-service.clearkey
