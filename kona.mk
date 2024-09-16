e470130f2790b06923f29a32bd04dff534ac5b68
Fiqri Ardyansyah <fiqri191002@gmail.com>
2024-09-16 21:07:17 +0700

    sm8250-common: Kill log spam in logcat from SensorService
    --------- beginning of main
    09-16 20:02:20.468  1651  2292 W SensorService: sensor 000004f8 already enabled in connection 0xb4000077d6d5f340 (ignoring)
    09-16 20:02:20.468  1651  2466 W SensorService: sensor 000004f8 already enabled in connection 0xb4000077d6d5f340 (ignoring)
    09-16 20:02:20.468  1651  2374 W SensorService: sensor 000004f8 already enabled in connection 0xb4000077d6d5f340 (ignoring)
    09-16 20:02:20.468  1651  2454 W SensorService: sensor 000004f8 already enabled in connection 0xb4000077d6d5f340 (ignoring)
    09-16 20:02:20.468  1651  2293 W SensorService: sensor 000004f8 already enabled in connection 0xb4000077d6d5f340 (ignoring)
    09-16 20:02:20.469  1651  2466 W SensorService: sensor 000004f8 already enabled in connection 0xb4000077d6d5f340 (ignoring)
    09-16 20:02:20.469  1651  2374 W SensorService: sensor 000004f8 already enabled in connection 0xb4000077d6d5f340 (ignoring)
    09-16 20:02:20.469  1651  2292 W SensorService: sensor 000004f8 already enabled in connection 0xb4000077d6d5f340 (ignoring)
    09-16 20:02:20.469  1651  2454 W SensorService: sensor 000004f8 already enabled in connection 0xb4000077d6d5f340 (ignoring)
    09-16 20:02:20.469  1651  2293 W SensorService: sensor 000004f8 already enabled in connection 0xb4000077d6d5f340 (ignoring)
    09-16 20:02:20.469  1651  2374 W SensorService: sensor 000004f8 already enabled in connection 0xb4000077d6d5f340 (ignoring)
    09-16 20:02:20.469  1651  2466 W SensorService: sensor 000004f8 already enabled in connection 0xb4000077d6d5f340 (ignoring)
    09-16 20:02:20.469  1651  2292 W SensorService: sensor 000004f8 already enabled in connection 0xb4000077d6d5f340 (ignoring)
    09-16 20:02:20.469  1651  2454 W SensorService: sensor 000004f8 already enabled in connection 0xb4000077d6d5f340 (ignoring)
    09-16 20:02:20.469  1651  2293 W SensorService: sensor 000004f8 already enabled in connection 0xb4000077d6d5f340 (ignoring)
    09-16 20:02:20.469  1651  2466 W SensorService: sensor 000004f8 already enabled in connection 0xb4000077d6d5f340 (ignoring)
    09-16 20:02:20.469  1651  2374 W SensorService: sensor 000004f8 already enabled in connection 0xb4000077d6d5f340 (ignoring)
    09-16 20:02:20.469  1651  2292 W SensorService: sensor 000004f8 already enabled in connection 0xb4000077d6d5f340 (ignoring)
    09-16 20:02:20.469  1651  2454 W SensorService: sensor 000004f8 already enabled in connection 0xb4000077d6d5f340 (ignoring)
    09-16 20:02:20.469  1651  2293 W SensorService: sensor 000004f8 already enabled in connection 0xb4000077d6d5f340 (ignoring)
    09-16 20:02:20.469  1651  2466 W SensorService: sensor 000004f8 already enabled in connection 0xb4000077d6d5f340 (ignoring)
    09-16 20:02:20.469  1651  2374 W SensorService: sensor 000004f8 already enabled in connection 0xb4000077d6d5f340 (ignoring)
    09-16 20:02:20.469  1651  2292 W SensorService: sensor 000004f8 already enabled in connection 0xb4000077d6d5f340 (ignoring)
    09-16 20:02:20.469  1651  2454 W SensorService: sensor 000004f8 already enabled in connection 0xb4000077d6d5f340 (ignoring)
    09-16 20:02:20.469  1651  2293 W SensorService: sensor 000004f8 already enabled in connection 0xb4000077d6d5f340 (ignoring)
    09-16 20:02:20.469  1651  2466 W SensorService: sensor 000004f8 already enabled in connection 0xb4000077d6d5f340 (ignoring)
    09-16 20:02:20.469  1651  2374 W SensorService: sensor 000004f8 already enabled in connection 0xb4000077d6d5f340 (ignoring)
    09-16 20:02:20.469  1651  2292 W SensorService: sensor 000004f8 already enabled in connection 0xb4000077d6d5f340 (ignoring)
    ...
    
    Signed-off-by: Fiqri Ardyansyah <fiqri191002@gmail.com>

--- kona.mk
+++ kona.mk
@@ -336,6 +336,9 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl-xiaomi \
     android.hardware.sensors@1.0-service
 
+PRODUCT_PACKAGES += \
+    SensorService
+
 #ADB AND USB DEBUGGING ON BOOT
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.usb.config=mtp,adb \
@@ -355,7 +358,6 @@ PRODUCT_SOONG_NAMESPACES += \
 
 # Speed profile services and wifi-service to reduce RAM and storage
 PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
-
 
 # Telephony
 PRODUCT_PACKAGES += \
