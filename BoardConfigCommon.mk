c62e496b8fc14f05f9d7f5db2b4e9d1be6093162
Yumi Yukimura <me.cafebabe@gmail.com>
2025-08-30 23:45:15 +0800

    sm8250-common: Cleanup more flags
    These are gone after bp conversion for display and media
    
    Change-Id: I1169adf12d717417f5f328ebbcc01ecc9b911889

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -95,9 +95,6 @@ TARGET_KERNEL_CONFIG := \
     vendor/debugfs.config \
     vendor/xiaomi/sm8250-common.config
 
-# Media
-TARGET_USES_ION := true
-
 # Partitions
 BOARD_FLASH_BLOCK_SIZE := 262144 # (BOARD_KERNEL_PAGESIZE * 64)
 ifeq ($(TARGET_IS_VAB),true)
