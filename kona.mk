6b9a479c04b508f5fb452ecd14109c2e0a328162
zenzer0s <praveenzalaki200@gmail.com>
2025-09-27 20:55:14 +0530

    sm8250-common: Enable ADB and USB debugging on boot

--- kona.mk
+++ kona.mk
@@ -334,6 +334,13 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl-xiaomi \
     android.hardware.sensors@1.0-service
 
+#ADB AND USB DEBUGGING ON BOOT
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.sys.usb.config=mtp,adb \
+    ro.adb.secure=0 \
+    ro.secure=0 \
+    ro.debuggable=1
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
