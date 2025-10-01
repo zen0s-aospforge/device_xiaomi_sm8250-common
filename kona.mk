6261fea054d9c255bf5c046c6496cde43c5e16b2
zenzer0s <praveenzalaki200@gmail.com>
2025-10-01 19:08:12 +0530

    Reapply "sm8250-common: remove OMX"
    *Revert was done to fix OMX Dolby
    Since we are using C2 Dolby Now So remove OMX
    
    This reverts commit e4f2f5f62cd2b1a0cb4e49244602523059bc3f07.

--- kona.mk
+++ kona.mk
@@ -255,8 +255,6 @@ $(call soong_config_set,lineage_health,charging_control_supports_bypass,false)
 PRODUCT_PACKAGES += \
     media_codecs_c2.xml \
     media_codecs_performance_c2.xml \
-    media_codecs_kona.xml \
-    media_codecs_performance_kona.xml \
     video_system_specs.json
 
 # NFC
@@ -276,11 +274,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_nfc/com.nxp.mifare.xml
 
 # OMX
-PRODUCT_PACKAGES += \
-    libOmxCore \
-    libOmxVdec \
-    libOmxVenc \
-    libstagefrighthw
+TARGET_SUPPORTS_OMX_SERVICE := false
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
