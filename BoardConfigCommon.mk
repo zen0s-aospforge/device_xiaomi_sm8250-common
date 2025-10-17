2bbe8263c8e7bc6ae759593ca86d9b410d50096a
zenzer0s <praveenzalaki200@gmail.com>
2025-10-18 00:41:33 +0530

    sm8250-common: Build Dolby Atmos
    Change-Id: Ie5e43bb7b58dd97447f9990ff76499787a3ceab9

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -212,7 +212,7 @@ BOARD_MOVE_GSI_AVB_KEYS_TO_VENDOR_BOOT := true
 endif
 
 # VINTF
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml \
     vendor/lineage/config/device_framework_matrix.xml
