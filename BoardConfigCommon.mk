a26ba07f28c0e7dfa82f7a5ea723593e99e9337a
zenzer0s <praveenzalaki200@gmail.com>
2025-10-19 15:35:35 +0530

    sm8250-common: Fix path for framework compatibility matrix in BoardConfigCommon.mk

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -249,7 +249,7 @@ endif
 
 # VINTF
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
-    $(COMMON_PATH)config/hidl/framework_compatibility_matrix.xml \
+    $(COMMON_PATH)/configs/hidl/framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/configs/hidl/manifest.xml
