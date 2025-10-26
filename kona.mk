3436eca84b1a26dbbfb9c00c701fcafeb9329342
zenzer0s <praveenzalaki200@gmail.com>
2025-10-26 14:20:54 +0530

    sm8250-common: add support for ViPER4Android FX
    - Use TARGET_INCLUDE_VIPERFX := true in lineage_$device.mk

--- kona.mk
+++ kona.mk
@@ -7,6 +7,9 @@
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
+# ViPER4Android FX
+$(call inherit-product, packages/apps/ViPER4AndroidFX/config.mk)
+
 # Add common definitions for Qualcomm
 $(call inherit-product, hardware/qcom-caf/common/common.mk)
 
